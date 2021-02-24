f9c53c83765d2aa73d8535de24207360349bac19
Rowin <rowin@anhydride-acide.fr>
2021-02-24 20:43:03 +0100

    Admin support

--- eval/admin.py
+++ eval/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TrainingSession, Student, Skill, CaseStudy, Evaluation
+
+
+class EvaluationInline(admin.TabularInline):
+    model = Evaluation
+
+
+class CaseStudyAdmin(admin.ModelAdmin):
+    inlines = (EvaluationInline,)
+
+
+admin.site.register(TrainingSession)
+admin.site.register(Student)
+admin.site.register(Skill)
+admin.site.register(CaseStudy, CaseStudyAdmin)
+admin.site.register(Evaluation)
