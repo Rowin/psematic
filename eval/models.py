018212274fd2c241a36bbac0ebd7e575b3aef02b
Rowin <rowin@anhydride-acide.fr>
2021-02-25 00:33:38 +0100

    Fix #4

--- eval/models.py
+++ eval/models.py
@@ -14,17 +14,19 @@ class Student(models.Model):
     mail = models.EmailField()
 
     def __str__(self):
-        return f'{self.firstname} {self.lastname.upper()}'
+        return f"{self.firstname} {self.lastname.upper()}"
 
 
 class Skill(models.Model):
-    training_session = models.ForeignKey("TrainingSession", on_delete=models.CASCADE)
+    training_session = models.ForeignKey(
+        "TrainingSession", on_delete=models.CASCADE, related_name="skills"
+    )
     description = models.TextField()
     order = models.IntegerField()
     parent = models.ForeignKey("Skill", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return f'{self.order}. {self.description}'
+        return f"{self.order}. {self.description}"
 
 
 class CaseStudy(models.Model):
@@ -41,14 +43,23 @@ class Evaluation(models.Model):
     comment = models.TextField(blank=True)
     skills = models.ManyToManyField(Skill, through="SkillEvaluation")
 
+    def save(self, *args, **kwargs):
+        is_new = self.id is None
+
+        # Save needed before Many-to-Many manipulation: an id is needed
+        super().save(*args, **kwargs)
+
+        # If new, the session skills are automatically linked to the Evaluation
+        if is_new:
+            for session_skill in self.case_study.training_session.skills.all():
+                skill_evaluation = SkillEvaluation(skill=session_skill, evaluation=self)
+                skill_evaluation.save()
+
 
 class SkillEvaluation(models.Model):
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
     evaluation = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
-    grade = models.TextField(choices=[
-        ('A', 'A'),
-        ('B', 'B'),
-        ('C', 'C'),
-        ('D', 'D')
-    ])
-    has_been_evaluated = models.BooleanField()
+    grade = models.TextField(
+        choices=[("A", "A"), ("B", "B"), ("C", "C"), ("D", "D")], blank=True
+    )
+    has_been_evaluated = models.BooleanField(default=False)
